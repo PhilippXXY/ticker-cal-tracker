beb53941e3c7f89bfcb1e810b63d14d0d48ef435
Philipp Schmidt <104377374+PhilippXXY@users.noreply.github.com>
2025-12-11 18:36:25 +0000

    Tick version

--- src/app/main.py
+++ src/app/main.py
@@ -60,7 +60,7 @@ def create_app():
     # Configuration for Flask-Smorest
     app.config.update({
         'API_TITLE': 'Ticker Calendar Tracker API',
-        'API_VERSION': '0.1.6',
+        'API_VERSION': '0.1.7',
         'OPENAPI_VERSION': '3.0.3',
         'OPENAPI_URL_PREFIX': '/',
         'OPENAPI_SWAGGER_UI_PATH': '/docs',
@@ -130,10 +130,3 @@ if __name__ == '__main__':
         port=port,
         debug=False,
     )
-    port = int(os.environ.get("PORT", 8080))
-    app.run(
-        host='0.0.0.0',
-        port=port,
-        debug=False,
-    )
-    
